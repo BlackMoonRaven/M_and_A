74d6936e587ead7a545a736a0f510ba81944e2f5
Andrii Politychka <dykozor2424@gmai.com>
2023-02-01 10:20:01 +0200

    Add project files.

---
+++ M_and_A/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace M_and_A.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
